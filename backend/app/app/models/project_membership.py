cb0dfe55c5752625c69845da9bc46ad0cea9ca8b
andrew <off-rey@bk.ru>
2021-08-14 21:56:15 +0300

    Add unique together to (user_id, project_id) tuple in project membership model.

--- backend/app/app/models/project_membership.py
+++ backend/app/app/models/project_membership.py
@@ -2,6 +2,7 @@ from typing import TYPE_CHECKING
 
 from sqlalchemy import Column, ForeignKey, Integer, Boolean
 from sqlalchemy.orm import relationship
+from sqlalchemy.schema import UniqueConstraint
 
 from app.db.base_class import Base
 
@@ -18,3 +19,6 @@ class ProjectMembership(Base):
     project = relationship("Project", back_populates="members")
     accepted = Column(Boolean, default=False)
 
+    __table_args__ = (
+        UniqueConstraint("user_id", "project_id", name="membership_uniq"),
+    )
