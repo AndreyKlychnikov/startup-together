fbc69f7fab14ffb9fa93ece256e66d51517d067a
andrew <off-rey@bk.ru>
2021-08-14 18:30:10 +0300

    Add project api entrypoint.

--- backend/app/app/schemas/__init__.py
+++ backend/app/app/schemas/__init__.py
@@ -1,4 +1,5 @@
 from .item import Item, ItemCreate, ItemInDB, ItemUpdate
+from .project import Project, ProjectCreate, ProjectInDB, ProjectUpdate
 from .msg import Msg
 from .token import Token, TokenPayload
 from .user import User, UserCreate, UserInDB, UserUpdate
