b60e5a7ed5a6116ae30c83b58680762e03499961
doker696 <doker696@users.noreply.github.com>
2021-09-01 23:34:40 +0300

    created simple projectItem component

---
+++ frontend/src/views/main/explore/ProjectItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{ project.title }}
+  </div>
+</template>
+
+<script lang='ts'>
+import { Component, Vue } from "vue-property-decorator";
+import { IProjectItem } from "@/interfaces";
+
+@Component({
+    props:[
+        'project'
+    ]
+})
+export default class ProjectItem extends Vue {
+    project!: IProjectItem
+}
+</script>
