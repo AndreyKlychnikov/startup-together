3f8f728978f594eb6324492784dc321b4c1a968e
doker696 <doker696@users.noreply.github.com>
2021-09-02 12:15:55 +0300

    added vuetify

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,6 +3,7 @@ import '@babel/polyfill';
 import './component-hooks';
 import Vue from 'vue';
 import './plugins/vuetify';
+import Vuetify from 'vuetify';
 import './plugins/vee-validate';
 import App from './App.vue';
 import router from './router';
@@ -15,5 +16,6 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
+  vuetify : new Vuetify(),
   render: (h) => h(App),
 }).$mount('#app');
