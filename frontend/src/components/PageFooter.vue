613d1490c7d41658218ddccbfcb4e22496a3b761
doker696 <doker696@users.noreply.github.com>
2021-09-02 12:19:16 +0300

    created footer component

---
+++ frontend/src/components/PageFooter.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-footer bottom absolute padless app>
+    <v-col class="text-center" cols="12">
+      {{ new Date().getFullYear() }} — <strong>StartUp-Together</strong>
+    </v-col>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      icons: [
+        "mdi-instagram",
+        "fab fa-twitter",
+        "fab fa-google-plus",
+        "fab fa-linkedin",
+        "fab fa-instagram",
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
