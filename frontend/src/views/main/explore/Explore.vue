f8c39fda62a1354aa38bd4d32f54f4fc86d2eb6e
doker696 <doker696@users.noreply.github.com>
2021-09-03 21:08:32 +0300

    changed grid of explore page

--- frontend/src/views/main/explore/Explore.vue
+++ frontend/src/views/main/explore/Explore.vue
@@ -1,58 +1,80 @@
 <template>
-  <div>
-    <v-row class="mt-6" no-gutters>
-      <v-col></v-col>
+  <v-container>
+    <div class="text-h4">Projects list</div>
+    <v-divider />
+    <v-container>
+      <v-row>
+        <v-col cols="9">
+          <project
+            v-for="data in datas"
+            :key="data.id"
+            :project="data"
+            class="my-2"
+          />
+        </v-col>
 
-      <v-col cols="6">
-        <project-item v-for="data in datas" :key="data.id" :project="data" />
-      </v-col>
-
-      <v-col class="center--text"> filter column </v-col>
-    </v-row>
-  </div>
+        <v-col>
+          <v-card class="mt-2">
+            <v-card-title>filter</v-card-title>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-container>
 </template>
 
 <script lang='ts'>
 import { Component, Vue } from "vue-property-decorator";
-import projectItem from "@/views/main/explore/ProjectItem.vue";
+import project from "@/views/main/explore/ProjectItem.vue";
 
 @Component({
   components: {
-    projectItem,
+    project,
   },
 })
 export default class Explore extends Vue {
   datas = [
     {
       title: "string1",
-      description: "string",
+      description: "Project 1 description",
       id: 0,
       owner_id: 0,
       members: [
         {
           user: {
-            email: "user@example.com",
+            email: "user1@example.com",
             is_active: true,
             is_superuser: false,
-            full_name: "string",
+            full_name: "user1",
             id: 0,
           },
           accepted: true,
         },
       ],
-    },{
+    },
+    {
       title: "string2",
-      description: "string",
-      id: 0,
-      owner_id: 0,
+      description: "Project 2 description",
+      id: 1,
+      owner_id: 1,
       members: [
         {
           user: {
-            email: "user@example.com",
+            email: "user1@example.com",
             is_active: true,
             is_superuser: false,
-            full_name: "string",
+            full_name: "user1",
             id: 0,
+          },
+          accepted: true,
+        },
+        {
+          user: {
+            email: "user2@example.com",
+            is_active: true,
+            is_superuser: false,
+            full_name: "user2",
+            id: 1,
           },
           accepted: true,
         },
