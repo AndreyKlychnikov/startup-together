c7709cd4e798bc81bd083ee2b576de08fac30659
doker696 <doker696@users.noreply.github.com>
2021-09-03 21:09:12 +0300

    changet project item layout

--- frontend/src/views/main/explore/ProjectItem.vue
+++ frontend/src/views/main/explore/ProjectItem.vue
@@ -1,19 +1,27 @@
 <template>
-  <div>
-    {{ project.title }}
-  </div>
+  <v-card>
+    <v-card-title>
+      <router-link class="text-decoration-none" :to="'project/id' + project.id.toString()">
+        {{ project.title }}
+      </router-link>
+    </v-card-title>
+    <v-card-subtitle>{{
+      project.members.find((member) => member.user.id == project.owner_id).user
+        .full_name
+    }}</v-card-subtitle>
+    <v-card-text>{{ project.description }}</v-card-text>
+    <v-card-actions> </v-card-actions>
+  </v-card>
 </template>
 
 <script lang='ts'>
 import { Component, Vue } from "vue-property-decorator";
-import { IProjectItem } from "@/interfaces";
+import { IProject } from "@/interfaces";
 
 @Component({
-    props:[
-        'project'
-    ]
+  props: ["project"],
 })
 export default class ProjectItem extends Vue {
-    project!: IProjectItem
+  project!: IProject;
 }
 </script>
