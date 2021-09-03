745a63bfa184e16480cdf36972da1fc422421dfa
doker696 <doker696@users.noreply.github.com>
2021-09-03 21:09:36 +0300

    created components for project page

---
+++ frontend/src/views/main/projects/Project.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        Project with id {{this.$route.params.id}}
+    </div>
+</template>
+
+<script lang='ts'>
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Project extends Vue {
+
+}
+</script>
---
+++ frontend/src/views/main/projects/Projects.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        Projects page
+    </div>
+</template>
+
+<script lang='ts'>
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Projects extends Vue {
+
+}
+</script>
